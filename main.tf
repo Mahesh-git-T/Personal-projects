97e527283d8391f2d730bda8c3e3d673febe2f74
Mahesh-git-T <mahesh73019@gmail.com>
2024-07-04 23:48:32 +0530

    Used variables in main

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "Azure_rg" {
   name     = "Dev_rg"
-  location = "South India"
+  location = var.location
   tags = {
     environment = "dev"
   }
